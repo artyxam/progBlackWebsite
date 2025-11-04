6245360f203db816ded100018a554198359b44e0
artyxam <188327131+artyxam@users.noreply.github.com>
2025-11-04 09:05:15 +0000

    added js

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,20 @@
-const nums = [3, 7, 2, 9, 12, 5, 8];
+var coll = document.getElementsByClassName("collapsible");
+    var i;
 
-function findMaxAndMin(nums){ 
-    let max = nums[0];
-    let min = nums[0];
-    for(let i = 1; i< nums.length; i++){
-        if(nums[i] > max;){
-            max = numbs[i];
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight){
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
         }
-        if (nums[i] < min){
-            min = nums[i];
-        }
-        }
-    return {max, min}
+        });
     }
-}
 
-const result = findMaxAndMin(nums);
-console.log(`Max: ${result.max}, Min: ${result.min}`);
+// Hamburger menu toggle
+document.getElementById("hamburger").addEventListener("click", function() {
+    var navLinks = document.getElementById("navLinks");
+    navLinks.classList.toggle("show");
+});
